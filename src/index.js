96fe100b1f0e39d814e1d32679d778d28a8a4d50
Felipe Passos <passos.fe@gmail.com>
2019-09-16 21:54:52 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Import React and ReactDOM libraries
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Create a React Component
+const App = () => {
+    return <div>Ola</div>;
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+//take the react component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
